a787e6f2b8574eb8e6319efd26fac98d4965e7e3
mrtSahin <mrtsvs07@gmail.com>
2022-07-06 22:53:06 +0300

    promise-fetch api

--- day18.js
+++ day18.js
@@ -44,7 +44,7 @@ const promise2=new Promise((resolve,reject)=>{
 
 
 // anne mutluysa çocuğuna telefon alacak
-let isMomHappy=false
+let isMomHappy=true
 
 let willGetNewPhone = new Promise(function(resolve,reject){
     if(isMomHappy){ // eğer anne mutluysa telefonu alıyor
@@ -82,8 +82,138 @@ const askMom =function(){
 
 // çocuk telefonunu arkadaşlarına göstericek
 const showToFriends=function(phone){
-    const message ="Hey friends this is my phone"+phone.name
+    const message ="Hey friends this is my phone "+phone.name
     return Promise.resolve(message) // burda da biz harici olarak bir resolve yollayabiliyoruz
 }
 
 askMom()
+
+
+
+// https://www.muratoner.net/javascript/javascript-fetch-api-nedir-http-nasil
+
+const url = 'https://restcountries.com/v2/all' // ülkeler apisi
+fetch(url) // asenkron veri alıp ve veri göndermemizi sağlar. Promise tabanlıdır
+    .then(function(response){
+        //console.log(response) // response nesnesinin özellikleri ve metodları
+        return response.json()
+    }) // veri json ise .json() text ise .text() metodunu kullanıyoruz
+    .then(data => { 
+        console.log(data)
+    })
+    .catch(error => console.error(error)) // handling error if something wrong happens
+
+// üstteki işlem GET isteğiydi 
+// POST isteği
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {  
+//     method: 'post',  
+//     headers: {  
+//       "Content-type": "application/json; charset=UTF-8"  
+//     },  
+//     body: {
+//       "userId": 1,
+     
+//       "title": "title test",
+//       "body": "body test"
+//     }
+//   }).then(response => response.json())
+// .then(data=>console.log(data))
+// .catch(error=>console.log(new Error("Hata")))
+
+
+// async and await
+// bir fonksiyonun önündeki async bu metodun bir söz verdiğini söyler.
+// sözden gelen değere erişmek içinse await i kullanırız
+// https://webolog.net/javascript-fetch-api-nedir-ve-nasil-kullanilir/#close
+async function fetchText() {
+    let response = await fetch('/readme.txt'); // readme.txt isminde bir dosya olmadığından hata verecek 
+    if(response.status===200){ // İstenen kaynak mevcut değilse, yanıt kodu 404‘tür:
+        let data = await response.text();
+        console.log(data);
+    }if(response.status===404){
+        console.log(new Error("Text bulunamadı")) // Error: Text bulunamadı  at fetchText (day18.js:135:21)
+        console.log(response.status); // 404
+        console.log(response.statusText); // Not Found
+    }
+
+}
+fetchText()
+// Response nesnesi, status ve statusText özellikleri aracılığıyla durum kodunu ve durum metnini 
+// sağlar. Bir istek başarılı olduğunda durum kodu 200‘dür ve durum metni OK‘dur:
+
+async function fetchText2(){
+    let response = await fetch(url);
+    console.log(response.status); // 200
+    console.log(response.statusText); // 
+    if(response.status===200){
+        let data  = await response.json();
+        console.log(data);
+        console.log("Bağlantı başarılı") // Bağlantı başarılı
+    }
+}
+fetchText2()
+
+
+// ülkeler API sini fetch kullanarak içeriğini alın ve consola country, capital, languages, population ve area
+// değerlerini basın  
+const countriesAPI = 'https://restcountries.com/v2/all'
+async function fetchCountires(API){
+    let response = await fetch(API);
+    if(response.status===200){
+        let data = await response.json()
+        data.forEach(country => {
+            let languagesStr=""
+            for(lang of country.languages){
+                languagesStr+=lang.name+" "
+            }
+            console.log(`Country: ${country.name} Capital: ${country.capital} Languages: ${languagesStr} Population: ${country.population} Area: ${country.area}`)
+        });
+        
+        // en kalabalık 10 ülke
+        data.sort((a,b)=>b.population-a.population)
+        console.log("%cen kalabalık 10 ülke","color:cyan")
+        for(let i=0;i<10;i++){
+            console.log(data[i])
+        }
+
+        // dünyada resmi olarak kullanılan toplam dil sayısı
+        let languages=[]
+        data.forEach(country => {   
+            for(lang of country.languages){
+                languages.push(lang.name)
+            }
+        });
+        //console.log(languages)
+        const languagesSet=new Set(languages)
+        console.log(`Dünyada konuşulan dil sayısı ${languagesSet.size}`)
+        
+    }
+}
+fetchCountires(countriesAPI)
+
+
+
+// Tüm kedi adlarını catNames değişkenine yazdırın.
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+function fetchCats(API){
+    fetch(catsAPI)
+    .then(response => {
+       return response.json()
+    })
+    .then(data =>{
+        console.log(data)
+        data.forEach(cat => {
+            console.log(cat.name+"    weight(metric) :"+cat.weight.metric)
+        });
+    })
+}
+
+fetchCats(catsAPI)
+
+
+
+
+
+
---
+++ day19.js
@@ -0,0 +1,88 @@
+// closure
+// java script de istediğimiz kadar iç içe fonksiyon yazabiliriz
+// bu durumda iç(inner) fonksiyonun , dış(outer) fonksiyonun değişkenlerine erişmesine closure denir.
+
+
+
+function outerFunction(){
+    let array=['a','b','c']
+    console.log(array) // ['a', 'b', 'c']
+    function innerFunction(){
+        array.push('d') // burdaki değişikliğin kapsam alanı bu iç fonksiyon olduğundan bu fonksiyon dışında array eski halini alacaktır
+        array.unshift('0')
+        console.log(array) // ['0', 'a', 'b', 'c', 'd']
+        return array
+    }
+    console.log(array) // ['a', 'b', 'c']
+    return innerFunction()
+}
+
+console.log(outerFunction()) // ['0', 'a', 'b', 'c', 'd']
+
+
+
+
+function arrayFunction(){
+    let num1=55
+    let num2=79
+
+    function sum(){ 
+        return num1+num2
+    }
+
+    function divide(){
+        return num1/num2
+    }
+
+    return {
+        added:sum(),
+        divided:divide()
+    }
+}
+
+console.log(arrayFunction().added)
+console.log(arrayFunction().divided)
+
+
+
+function personAccount(){
+    const firstName = "Murat"
+    const lastName = "Şahin"
+    const incomes = [100,50,200]
+    const expense = [20,50,65,100,30]
+
+    function totalIncome(){
+        return incomes.reduce((acc,price)=> acc+price)
+    }
+
+    function totalExpense(){
+
+    }
+
+    function accountInfo(){
+
+    }
+
+    function addIncome(){
+
+    }
+
+    function addExpense(){
+
+    }
+
+    function accountsBalance(){
+
+    }
+    return totalIncome()
+}
+
+console.log(personAccount())
+
+
+
+
+
+
+
+
